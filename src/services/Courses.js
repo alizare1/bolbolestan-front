4bb45245bc9da4aee35fff148bdca02abf501775
alizare1 <ali.zare2000123@gmail.com>
2021-04-26 11:10:15 +0430

    update api urls

--- src/services/Courses.js
+++ src/services/Courses.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export async function getCourses(filter, type) {
     const resp = await axios.get(
-        `http://localhost:8080/courses?${filter ? "filter=" + filter + "&" : ""}${type ? 'type=' + type : ""}`
+        `http://localhost:8080/api/courses?${filter ? "filter=" + filter + "&" : ""}${type ? 'type=' + type : ""}`
     );
     return resp.data;
 }
 
 export async function getCourse(code, group) {
     const resp = await axios.get(
-        `http://localhost:8080/courses/${code}/${group}`
+        `http://localhost:8080/api/courses/${code}/${group}`
     );
     return resp.data;
 }
