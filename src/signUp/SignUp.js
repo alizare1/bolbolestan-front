cc3ff75c9ce1531d8a1693373fa28130678e7ecb
Melika Morafegh <melikamrf@gmail.com>
2021-05-17 17:55:30 +0430

    add label to input

--- src/signUp/SignUp.js
+++ src/signUp/SignUp.js
@@ -1,17 +1,36 @@
 import {Fragment,useEffect,useState} from "react";
+import { useHistory, useParams } from "react-router";
 import { useForm } from "react-hook-form";
 
 
 import '../login/login.css';
+import {getUsername, saveToken, saveUsername} from "../services/SessionUtils";
+import {toast} from "react-toastify";
+import {signup} from "../services/Auth";
 
 
 function SignUp(props) {
     useEffect(() => document.title = 'ثبت نام',[]);
+
     const { register, handleSubmit, errors } = useForm();
+
+    const history = useHistory();
 
     const onSubmit = (data) => {
         let a = {...data, status: 'مشغول به تحصیل', img: 'http://138.197.181.131:5200/img/claire_wheeler.jpg'};
         console.log(a);
+        signup(a).then(resp => {
+            console.log(resp);
+            toast.success('ثبت نام با موفقيت انجام شد')
+            history.push('/login');
+        }
+        ).catch(e => {
+            console.log(e);
+            if (e.response)
+                toast.error(e.response.data.error);
+            else
+                toast.error('مشکل در ارتباط با سرور');
+        })
     }
 
     const btnStyle = {
@@ -25,15 +44,32 @@ function SignUp(props) {
                 <Fragment>
                     <div className="sign">ثبت نام</div>
                     <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                        <input {...register('name', {required: true})} name='name' className="form-input" type="text"  placeholder="نام" />
-                        <input {...register('secondName', {required: true})} name='secondName' className="form-input" type="text"  placeholder="نام خانوادگی" />
-                        <input {...register('id', {required: true, minLength:9, maxLength:9})} name='id' className="form-input" type='text' pattern='[0-9]*'  placeholder="شماره دانشجویی" />
-                        <input {...register('birthDate', {required: true})} name='birthDate' className="form-input" type="text"  placeholder="تاریخ تولد" />
-                        <input {...register('field', {required: true})} name='field' className="form-input" type="text"  placeholder="رشته" />
-                        <input {...register('faculty', {required: true})} name='faculty' className="form-input" type="text"  placeholder="دانشکده" />
-                        <input {...register('level', {required: true})} name='level' className="form-input" type="text"  placeholder="مقطع" />
-                        <input {...register('email', {required: true})} name='email' className="form-input" type="email"  placeholder="ایمیل" />
-                        <input {...register('password', {required: true})} name='password' className="form-input" type="password"  placeholder="رمز عبور" />
+                        <label> نام</label>
+                        <input {...register('name', {required: true})} name='name' className="form-input" type="text" />
+
+                        <label>نام خانوادگی</label>
+                        <input {...register('secondName', {required: true})} name='secondName' className="form-input" type="text" />
+
+                        <label> شماره دانشجویی</label>
+                        <input {...register('id', {required: true, minLength:9, maxLength:9})} name='id' className="form-input" type='text' pattern='[0-9]*'   />
+
+                        <label>تاریخ تولد</label>
+                        <input {...register('birthDate', {required: true})} name='birthDate' className="form-input" type="text" />
+
+                        <label> رشته</label>
+                        <input {...register('field', {required: true})} name='field' className="form-input" type="text"  />
+
+                        <label> دانشکده</label>
+                        <input {...register('faculty', {required: true})} name='faculty' className="form-input" type="text" />
+
+                        <label>مقطع</label>
+                        <input {...register('level', {required: true})} name='level' className="form-input" type="text"  />
+
+                        <label>ایمیل</label>
+                        <input {...register('email', {required: true})} name='email' className="form-input" type="email"  />
+
+                        <label> رمز عبور</label>
+                        <input {...register('password', {required: true})} name='password' className="form-input" type="password"  />
                         <button style={btnStyle}  type="submit" className="submit-btn" >
                           ثبت نام
                         </button>
